0c538daceb0340602832d0a2c4c94602f28507c8
Jonathan Derek McArthur <jondmcarthur@gmail.com>
2021-08-17 16:34:36 -0500

    commit tree c5c803d1e2364b792ba1c14b20c77e7d559e6c08

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+const num3 = 19;
+const num4 = 5;
+let mod = num3 % num4;
+let max = Math.max(1, 2, 3, 20);
